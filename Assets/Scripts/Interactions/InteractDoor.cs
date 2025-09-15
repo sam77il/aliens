be971968eca3c1f772d4e0dbf5fc9e06f3fe1f84
BTLV444\Osman <2024.gme.osmansen@gmail.com>
2025-09-15 10:46:48 +0200

    ADD Interactions SCripts and made Screws, Vents and Doors interactable; Vents interaction depends on Screws, only if al screws for the vent are unscrewed the vent opens ADD Prefabs for Doors(Un/Openable) and vents ADD 2 Tags for the Doors

---
+++ Assets/Scripts/Interactions/InteractDoor.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+
+public class InteractDoor : Interactables
+{
+    private InteractDoor[] doors;
+
+    void Start()
+    {
+        // get the parallel gameobjects, including self, that have the Script InteractDoor and assign it to the doors variable
+        doors = new InteractDoor[transform.parent.childCount - 1];
+        int index = 0;
+        foreach (Transform sibling in transform.parent)
+        {
+            if (sibling.GetComponent<InteractDoor>() != null)
+            {
+                doors[index] = sibling.GetComponent<InteractDoor>();
+                index++;
+            }
+        }
+
+    }
+
+    public override void Use()
+    {
+        StartCoroutine(UseHelper());
+    }
+
+    private IEnumerator UseHelper()
+    {
+        // move door that has the Tag "DoorLeft" postitive in local x axis over 1 second and the door that has the Tag "DoorRight" negative in local x axis over 1 second
+        float sec = 0f;
+        while (sec <= 0.75f)
+        {
+            if (gameObject.CompareTag("DoorLeft"))
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * 1f, Space.Self);
+            }
+            else if (gameObject.CompareTag("DoorRight"))
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * 1f, Space.Self);
+            }
+            sec += Time.deltaTime;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        // disable this scripts for both doors
+        foreach (InteractDoor door in doors)
+        {
+            if (door != null)
+            {
+                door.enabled = false;
+            }
+        }
+    }
+
+}
